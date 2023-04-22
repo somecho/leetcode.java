2ac7d57f507603c22db6ac8279503ca4ea58c3e7
samuel cho <samonthepiano@gmail.com>
2023-04-22 22:38:28 +0200

    add return false clause

--- app/src/main/java/problems/palindromeNumber/Solution.java
+++ app/src/main/java/problems/palindromeNumber/Solution.java
@@ -5,6 +5,7 @@ public class Solution {
 		String y = String.valueOf(x);
 		int len = y.length();
 		int halfLen = len/2;
+		if (x < 0) return false;
 		for (int i = 0; i < halfLen; i++) {
 			if (y.charAt(i) != y.charAt(len - (i + 1))) {
 				return false;
