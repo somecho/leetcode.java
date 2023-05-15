2f46bdb838da42ad09f29ada203b5c2f29bf2e07
Somē Cho <itssomicho@gmail.com>
2023-05-15 09:10:47 +0200

    remove unnecessary second loop

--- app/src/main/java/plusOne/Solution.java
+++ app/src/main/java/plusOne/Solution.java
@@ -11,16 +11,8 @@ class Solution {
 				return digits;
 			}
 		}
-
-		if (digits[0] == 0) {
-			int[] output = new int[digits.length + 1];
-			output[0] = 1;
-			for (int i = 0; i < digits.length; i++) {
-				output[i + 1] = digits[i];
-			}
-			return output;
-		}
-
-		return new int[digits.length];
+		digits = new int[digits.length + 1];
+		digits[0] = 1;
+		return digits;
 	}
 }
